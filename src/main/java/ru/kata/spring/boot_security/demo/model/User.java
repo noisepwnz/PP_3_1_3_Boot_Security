d2ec3496c90a983dae7d2e100a3e50e67464f27b
nikolay <noisepwnz@mail.ru>
2022-09-14 00:03:52 +0500

    boossttrap

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -57,11 +57,11 @@ public class User implements UserDetails {
     }
 
 
-    @ManyToMany(cascade=CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @ManyToMany(cascade=CascadeType.REFRESH, fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles ;
 
 
     public int getId() {
